7a2e83def8f4f37ece69738e9749b39f6213a17d
anamika-c-11 <anamikaknr2026@gmail.com>
2026-08-31 12:52:27 +0530

    Clean up comments in runningLed.v
    Removed comments about LED state change timing and clock frequency.

--- runningLed.v
+++ runningLed.v
@@ -6,10 +6,6 @@ input [7:0] initState,
 output reg [7:0] led
 );
 
-//LEDs should change state once every second
-//Clock freq 100MHz -> period is 10ns
-//1/10ns = 100000000 clock cycles
-
 reg [30:0] counter;
 
 always @(posedge clock)
@@ -26,5 +22,6 @@ begin
         led <= initState;
     else if(counter == 100000000)
         led[7:0] <= {led[0],led[7:1]};
+    
 end
 endmodule
